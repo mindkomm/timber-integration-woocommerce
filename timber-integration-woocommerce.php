d9848af256ff72c2c78a763720b098bb514fe571
Lukas Gaechter <lukas.gaechter@mind.ch>
2017-08-17 08:23:52 +0200

    Add all files

---
+++ timber-integration-woocommerce.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Timber Integration for WooCommerce
+ * Plugin URI: https://bitbucket.org/mindkomm/timber-integration-woocommerce
+ * Description: WooCommerce Integration for WooCommerce
+ * Author: MIND
+ * Author URI: https://www.mind.ch
+ * License: GNU General Public License v2.0 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Version: 0.1.0
+ */
+namespace Mind;
+
+require_once( __DIR__ . '/vendor/autoload.php' );
