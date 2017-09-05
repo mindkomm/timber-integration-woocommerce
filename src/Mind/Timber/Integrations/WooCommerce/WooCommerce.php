099ce21ee30f6b2abb291458495ca3a55e387434
Lukas Gaechter <lukas.gaechter@mind.ch>
2017-09-05 16:45:05 +0200

    Add function to remove WooCommerce image actions by default

--- src/Mind/Timber/Integrations/WooCommerce/WooCommerce.php
+++ src/Mind/Timber/Integrations/WooCommerce/WooCommerce.php
@@ -256,4 +256,16 @@ class WooCommerce {
 
 		return $context;
 	}
+
+	/**
+	 * Disable image functions added in WooCommerce.
+	 *
+	 * This is useful if you want to use Timber’s image functionalities to display images.
+	 */
+	public static function disable_woocommerce_images() {
+		remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
+		remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20 );
+		remove_action( 'woocommerce_product_thumbnails', 'woocommerce_show_product_thumbnails', 20 );
+		remove_action( 'woocommerce_before_subcategory_title', 'woocommerce_subcategory_thumbnail', 10 );
+	}
 }
