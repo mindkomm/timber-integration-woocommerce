20592873d59fc52fb73c9047d38b589d63bf3200
Lukas Gaechter <lukas.gaechter@mind.ch>
2018-02-13 15:03:35 +0100

    Update plugin version

--- timber-integration-woocommerce.php
+++ timber-integration-woocommerce.php
@@ -5,6 +5,6 @@
  * Description: WooCommerce Integration for Timber
  * Author: Lukas Gaechter
  * Author URI: https://www.mind.ch
- * Version: 0.4.0
+ * Version: 0.5.0
  */
 require_once __DIR__ . '/vendor/autoload.php';
