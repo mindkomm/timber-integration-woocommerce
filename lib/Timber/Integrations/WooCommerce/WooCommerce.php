e7ff717bea50f76f6e306f7d346cbf1f616c6d00
pascalknecht <pascal@webkinder.ch>
2020-07-29 15:18:04 +0200

    Added wc_action

--- lib/Timber/Integrations/WooCommerce/WooCommerce.php
+++ lib/Timber/Integrations/WooCommerce/WooCommerce.php
@@ -323,6 +323,12 @@ class WooCommerce {
 		$twig->addFunction( new \Timber\Twig_Function( 'Product', function( $pid ) {
 			return new self::$product_class( $pid );
 		} ) );
+    $twig->addFunction( new \Timber\Twig_Function( 'wc_action', function() {
+      $args = func_get_args();
+      $action = $args[0];
+      array_shift( $args );
+      do_action_ref_array( $action, $args );
+    } ) );
 
 		return $twig;
 	}
